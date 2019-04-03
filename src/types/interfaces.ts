c70d5a67158ab1a633bbef805df47b4947f3f37b
James Kirk <jameskirk8@gmail.com>
2019-04-04 09:02:36 +1100

    rying to get debugger to work with env variables

---
+++ src/types/interfaces.ts
@@ -0,0 +1,12 @@
+export interface IEnvCustom extends NodeJS.ProcessEnv {
+  NODE_ENV: "development" | "production";
+  DATABASE: string;
+  MAIL_USER: string;
+  MAIL_PASS: string;
+  MAIL_HOST: string;
+  MAIL_PORT: string;
+  PORT: string;
+  MAP_KEY: string;
+  SECRET: string;
+  KEY: string;
+}
